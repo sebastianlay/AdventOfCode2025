9e9fd7d72e8312732a2de0a9f7036d5d7be649ac
Sebastian Lay <mail@sebastian-lay.de>
2025-12-02 23:46:29 +0100

    Adding day 1

---
+++ Day01Part01/Program.cs
@@ -0,0 +1,23 @@
+﻿var position = 50;
+var password = 0;
+
+var instructions = File.ReadAllLines("input");
+foreach (var instruction in instructions)
+{
+    if (instruction.Length < 2)
+        continue;
+
+    if (!int.TryParse(instruction[1..], out var offset))
+        continue;
+
+    if (instruction[0] == 'L')
+        offset *= -1;
+
+    position += offset;
+    position = (position % 100 + 100) % 100;
+
+    if (position == 0)
+        password++;
+}
+
+Console.WriteLine($"Password: {password}");
---
+++ Day01Part02/Program.cs
@@ -0,0 +1,32 @@
+﻿var position = 50;
+var password = 0;
+
+var instructions = File.ReadAllLines("input");
+foreach (var instruction in instructions)
+{
+    if (instruction.Length < 2)
+        continue;
+
+    if (!int.TryParse(instruction[1..], out var offset))
+        continue;
+
+    password += offset / 100;
+
+    if (instruction[0] == 'L')
+        offset *= -1;
+
+    var actual = offset % 100;
+    position += actual;
+
+    // cross zero when turning the dial right
+    if (position < actual || position >= 100)
+        password++;
+
+    // cross zero when turning the dial left
+    if (position <= 0 && position != actual)
+        password++;
+
+    position = (position % 100 + 100) % 100;
+}
+
+Console.WriteLine($"Password: {password}");
