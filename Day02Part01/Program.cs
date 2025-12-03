996dd7a7fe7147fe094f91575b3387b806ca6688
Sebastian Lay <mail@sebastian-lay.de>
2025-12-03 01:01:30 +0100

    Adding day 2

---
+++ Day02Part01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+var sum = 0L;
+var input = File.ReadLines("input").First();
+foreach (var range in input.Split(','))
+{
+    var ids = range.Split('-');
+    if (!long.TryParse(ids[0], out var min) || !long.TryParse(ids[1], out var max))
+        continue;
+
+    sum += Range(min, max).Where(IsInvalid).Sum();
+}
+
+Console.WriteLine($"Sum: {sum}");
+return;
+
+bool IsInvalid(long id)
+{
+    var str = id.ToString(CultureInfo.InvariantCulture);
+    var length = str.Length;
+    return length % 2 == 0 && str[..(length / 2)] == str[(length / 2)..];
+}
+
+IEnumerable<long> Range(long start, long end)
+{
+    while (start <= end)
+    {
+        yield return start;
+        start++;
+    }
+}
---
+++ Day02Part02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Day02Part02;
+
+internal static partial class Program
+{
+    [GeneratedRegex(@"^(.*)\1+$")]
+    private static partial Regex IsInvalidRegex();
+
+    public static void Main()
+    {
+        var sum = 0L;
+        var input = File.ReadLines("input").First();
+        foreach (var range in input.Split(','))
+        {
+            var ids = range.Split('-');
+            if (!long.TryParse(ids[0], out var min) || !long.TryParse(ids[1], out var max))
+                continue;
+
+            sum += Range(min, max).Where(IsInvalid).Sum();
+        }
+
+        Console.WriteLine($"Sum: {sum}");
+    }
+
+    private static bool IsInvalid(long id)
+    {
+        var str = id.ToString(CultureInfo.InvariantCulture);
+        return IsInvalidRegex().IsMatch(str);
+    }
+
+    private static IEnumerable<long> Range(long start, long end)
+    {
+        while (start <= end)
+        {
+            yield return start;
+            start++;
+        }
+    }
+}
