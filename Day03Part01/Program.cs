7b88b42a4e0ae5b1016a7812e50e0019c05a6486
Sebastian Lay <mail@sebastian-lay.de>
2025-12-03 12:44:35 +0100

    Adding day 3

---
+++ Day03Part01/Program.cs
@@ -0,0 +1,25 @@
+﻿var sum = 0;
+
+var banks = File.ReadLines("input");
+foreach (var bank in banks)
+{
+    var concatenated = string.Empty;
+    var firstDigit = bank.Max();
+    var firstDigitIndex = bank.IndexOf(firstDigit);
+
+    if (firstDigitIndex == bank.Length - 1)
+    {
+        var secondDigit = bank[..firstDigitIndex].Max();
+        concatenated = secondDigit.ToString() + firstDigit.ToString();
+    }
+    else
+    {
+        var secondDigit = bank[(firstDigitIndex + 1)..].Max();
+        concatenated = firstDigit.ToString() + secondDigit.ToString();
+    }
+
+    if (int.TryParse(concatenated, out var parsed))
+        sum += parsed;
+}
+
+Console.WriteLine($"Sum: {sum}");
---
+++ Day03Part02/Program.cs
@@ -0,0 +1,23 @@
+﻿const int rounds = 12;
+var sum = 0L;
+
+var banks = File.ReadLines("input");
+foreach (var bank in banks)
+{
+    var concatenated = string.Empty;
+    var cursor = 0;
+
+    for (var i = rounds - 1; i >= 0; i--)
+    {
+        var digit = bank[cursor..^i].Max();
+        var index = bank[cursor..^i].IndexOf(digit);
+
+        concatenated += digit;
+        cursor += index + 1;
+    }
+
+    if (long.TryParse(concatenated, out var parsed))
+        sum += parsed;
+}
+
+Console.WriteLine($"Sum: {sum}");
