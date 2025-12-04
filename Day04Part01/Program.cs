857d2796a17fe59959f9d2018a10cf1c1211bf78
Sebastian Lay <mail@sebastian-lay.de>
2025-12-04 20:46:04 +0100

    Adding day 4

---
+++ Day04Part01/Program.cs
@@ -0,0 +1,53 @@
+﻿var sum = 0u;
+
+var grid = File.ReadAllLines("input");
+var width = grid[0].Length;
+var height = grid.Length;
+
+for (var y = 0; y < height; y++)
+{
+    for (var x = 0; x < width; x++)
+    {
+        if (grid[y][x] == '.')
+            continue;
+
+        var adjacent = 0u;
+
+        // top left
+        if (x > 0 && y > 0 && grid[y - 1][x - 1] == '@')
+            adjacent++;
+
+        // top middle
+        if (y > 0 && grid[y - 1][x] == '@')
+            adjacent++;
+
+        // top right
+        if (y > 0 && x < width - 1 && grid[y - 1][x + 1] == '@')
+            adjacent++;
+
+        // middle left
+        if (x > 0 && grid[y][x - 1] == '@')
+            adjacent++;
+
+        // middle right
+        if (x < width - 1 && grid[y][x + 1] == '@')
+            adjacent++;
+
+        // bottom left
+        if (x > 0 && y < height - 1 && grid[y + 1][x - 1] == '@')
+            adjacent++;
+
+        // bottom middle
+        if (y < height - 1 && grid[y + 1][x] == '@')
+            adjacent++;
+
+        // bottom right
+        if (x < width - 1 && y < height - 1 && grid[y + 1][x + 1] == '@')
+            adjacent++;
+
+        if (adjacent < 4)
+            sum++;
+    }
+}
+
+Console.WriteLine($"Sum: {sum}");
---
+++ Day04Part02/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Day04Part02;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        var grid = File.ReadAllLines("input").Select(x => x.ToCharArray()).ToArray();
+        var width = grid[0].Length;
+        var height = grid.Length;
+        var sum = 0u;
+        var offsets = new (int dx, int dy)[]
+        {
+            (-1, -1), (0, -1), (1, -1),
+            (-1,  0),          (1,  0),
+            (-1,  1), (0,  1), (1,  1),
+        };
+
+        while (RemoveRolls(grid, width, height, offsets, ref sum));
+        Console.WriteLine($"Sum: {sum}");
+    }
+
+    private static bool RemoveRolls(char[][] grid, int width, int height, (int, int)[] offsets, ref uint sum)
+    {
+        var removed = 0u;
+
+        for (var y = 0; y < height; y++)
+        {
+            for (var x = 0; x < width; x++)
+            {
+                if (grid[y][x] != '@')
+                    continue;
+
+                var adjacent = 0u;
+
+                foreach (var (dx, dy) in offsets)
+                {
+                    if (grid.At(x + dx, y + dy) == '@')
+                        adjacent++;
+                }
+
+                if (adjacent >= 4)
+                    continue;
+
+                grid[y][x] = 'x';
+                removed++;
+            }
+        }
+
+        sum += removed;
+        return removed > 0;
+    }
+
+    private static char At(this char[][] grid, int x, int y)
+    {
+        if (y < 0 || y >= grid.Length)
+            return ' ';
+
+        if (x < 0 || x >= grid[y].Length)
+            return ' ';
+
+        return grid[y][x];
+    }
+}
