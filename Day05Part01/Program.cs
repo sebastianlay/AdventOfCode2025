6fb2775dbe17827682abed2c7ce0ff770ebd076a
Sebastian Lay <mail@sebastian-lay.de>
2025-12-05 22:30:06 +0100

    Adding day 5

---
+++ Day05Part01/Program.cs
@@ -0,0 +1,16 @@
+﻿var input = File.ReadAllLines("input");
+var empty = input.IndexOf(string.Empty);
+var ranges = new List<(long min, long max)>();
+
+foreach (var range in input[..empty])
+{
+    var split = range.Split('-');
+    if (long.TryParse(split[0], out var min) && long.TryParse(split[1], out var max))
+        ranges.Add((min, max));
+}
+
+var fresh = input[(empty + 1)..]
+    .Select(long.Parse)
+    .Count(i => ranges.Any(r => i >= r.min && i <= r.max));
+
+Console.WriteLine($"Fresh: {fresh}");
---
+++ Day05Part02/Program.cs
@@ -0,0 +1,36 @@
+﻿var input = File.ReadAllLines("input");
+var ranges = input.TakeWhile(i => i.Length > 0).Select(GetRange).ToList();
+while (MergeRanges());
+var fresh = ranges.Sum(range => range.max - range.min + 1);
+
+Console.WriteLine($"Fresh: {fresh}");
+return;
+
+(long min, long max) GetRange(string line)
+{
+    var split = line.Split('-');
+    var min = long.Parse(split[0], CultureInfo.InvariantCulture);
+    var max = long.Parse(split[1], CultureInfo.InvariantCulture);
+    return (min, max);
+}
+
+bool MergeRanges()
+{
+    for (var i = 1; i < ranges.Count; i++)
+    {
+        for (var j = 0; j < i; j++)
+        {
+            if (ranges[i].min > ranges[j].max || ranges[i].max < ranges[j].min)
+                continue;
+
+            var min = Math.Min(ranges[i].min, ranges[j].min);
+            var max = Math.Max(ranges[i].max, ranges[j].max);
+            ranges.RemoveAt(i);
+            ranges.RemoveAt(j);
+            ranges.Add((min, max));
+            return true;
+        }
+    }
+
+    return false;
+}
