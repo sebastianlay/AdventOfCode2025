7afeead82cf548d2168ca437c5088a8eb87c66ca
Sebastian Lay <mail@sebastian-lay.de>
2025-12-06 21:26:23 +0100

    Adding day 6

---
+++ Day06Part01/Program.cs
@@ -0,0 +1,28 @@
+﻿var input = File.ReadAllLines("input");
+const StringSplitOptions options = StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries;
+var grid = input.Select(line => line.Split(' ', options)).ToArray();
+var width = grid[0].Length;
+var height = grid.Length;
+var total = 0L;
+
+for (var x = 0; x < width; x++)
+{
+    var operand = grid[^1][x][0];
+    var subtotal = long.Parse(grid[0][x], CultureInfo.InvariantCulture);
+    for (var y = 1; y < height - 1; y++)
+    {
+        switch (operand)
+        {
+            case '+':
+                subtotal += long.Parse(grid[y][x], CultureInfo.InvariantCulture);
+                break;
+            case '*':
+                subtotal *= long.Parse(grid[y][x], CultureInfo.InvariantCulture);
+                break;
+        }
+    }
+
+    total += subtotal;
+}
+
+Console.WriteLine($"Total: {total}");
---
+++ Day06Part02/Program.cs
@@ -0,0 +1,47 @@
+﻿var input = File.ReadAllLines("input");
+var operations = new List<(char operand, int width)>();
+const int height = 4;
+
+var operand = input[height][0];
+var width = 1;
+for (var i = 1; i < input[height].Length; i++)
+{
+    width++;
+
+    if (i == input[height].Length - 1)
+    {
+        operations.Add((operand, width));
+        continue;
+    }
+
+    if (input[height][i] == ' ')
+        continue;
+
+    operations.Add((operand, width - 2));
+    operand = input[height][i];
+    width = 1;
+}
+
+var total = 0L;
+var offset = 0;
+foreach (var operation in operations)
+{
+    var subtotal = operation.operand == '+' ? 0L : 1L;
+    for (var i = 0; i < operation.width; i++)
+    {
+        var number = string.Empty;
+        for (var j = 0; j < height; j++)
+            number += input[j][i + offset];
+
+        var parsed = long.Parse(number, CultureInfo.InvariantCulture);
+        if (operation.operand == '+')
+            subtotal += parsed;
+        else
+            subtotal *= parsed;
+    }
+
+    total += subtotal;
+    offset += operation.width + 1;
+}
+
+Console.WriteLine($"Total: {total}");
