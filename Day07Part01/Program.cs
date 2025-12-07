a44c8d4f042865243ab134cc5a635cc8ff69856c
Sebastian Lay <mail@sebastian-lay.de>
2025-12-07 20:38:55 +0100

    Adding day 7

---
+++ Day07Part01/Program.cs
@@ -0,0 +1,23 @@
+﻿var input = File.ReadAllLines("input");
+var splits = 0;
+var width = input[0].Length;
+var start = input[0].IndexOf('S');
+var beams = new bool[width];
+beams[start] = true;
+
+for (var i = 2; i < input.Length; i += 2)
+{
+    var splitter = 0;
+    while ((splitter = input[i].IndexOf('^', ++splitter)) >= 0)
+    {
+        if (beams[splitter])
+        {
+            beams[splitter] = false;
+            beams[splitter - 1] = true;
+            beams[splitter + 1] = true;
+            splits++;
+        }
+    }
+}
+
+Console.WriteLine($"Splits: {splits}");
---
+++ Day07Part02/Program.cs
@@ -0,0 +1,22 @@
+﻿var input = File.ReadAllLines("input");
+var width = input[0].Length;
+var start = input[0].IndexOf('S');
+var beams = new long[width];
+beams[start] = 1;
+
+for (var i = 2; i < input.Length; i += 2)
+{
+    var splitter = 0;
+    while ((splitter = input[i].IndexOf('^', ++splitter)) >= 0)
+    {
+        if (beams[splitter] > 0)
+        {
+            beams[splitter - 1] += beams[splitter];
+            beams[splitter + 1] += beams[splitter];
+            beams[splitter] = 0;
+        }
+    }
+}
+
+var timelines = beams.Sum();
+Console.WriteLine($"Timelines: {timelines}");
