605747e9c3baafba63195f16bfb2d21f30d3c58c
Sebastian Lay <mail@sebastian-lay.de>
2025-12-09 13:15:01 +0100

    Adding day 9

---
+++ Day09Part01/Program.cs
@@ -0,0 +1,21 @@
+﻿var input = File.ReadAllLines("input");
+var tiles = new List<(int X, int Y)>(input.Length);
+
+foreach (var line in input)
+{
+    var parts = line.Split(',').Select(int.Parse).ToArray();
+    tiles.Add((parts[0], parts[1]));
+}
+
+var max = 0L;
+for (var i = 0; i < tiles.Count; i++)
+{
+    for (var j = i + 1; j < tiles.Count; j++)
+    {
+        var width = Math.Abs(tiles[i].X - tiles[j].X) + 1L;
+        var height = Math.Abs(tiles[i].Y - tiles[j].Y) + 1;
+        max = Math.Max(max, width * height);
+    }
+}
+
+Console.WriteLine($"Max: {max}");
---
+++ Day09Part02/Program.cs
@@ -0,0 +1,45 @@
+﻿var input = File.ReadAllLines("input");
+var tiles = new List<(int X, int Y)>(input.Length);
+var max = 0L;
+
+foreach (var line in input)
+{
+    var parts = line.Split(',').Select(int.Parse).ToArray();
+    tiles.Add((parts[0], parts[1]));
+}
+
+foreach (var rect in GetRects())
+{
+    if (GetEdges().Any(edge => Intersects(rect, edge)))
+        continue;
+
+    var width = Math.Abs(tiles[rect.A].X - tiles[rect.B].X) + 1L;
+    var height = Math.Abs(tiles[rect.A].Y - tiles[rect.B].Y) + 1;
+    max = Math.Max(max, width * height);
+}
+
+Console.WriteLine($"Max: {max}");
+return;
+
+IEnumerable<(int A, int B)> GetRects()
+{
+    for (var i = 0; i < tiles.Count; i++)
+        for (var j = i + 1; j < tiles.Count; j++)
+            yield return (i, j);
+}
+
+IEnumerable<(int A, int B)> GetEdges()
+{
+    for (var i = 0; i < tiles.Count; i++)
+        yield return (i, (i + 1) % tiles.Count);
+}
+
+bool Intersects((int A, int B) rect, (int A, int B) edge)
+{
+    var right = Math.Min(tiles[edge.A].X, tiles[edge.B].X) >= Math.Max(tiles[rect.A].X, tiles[rect.B].X);
+    var left  = Math.Max(tiles[edge.A].X, tiles[edge.B].X) <= Math.Min(tiles[rect.A].X, tiles[rect.B].X);
+    var above = Math.Min(tiles[edge.A].Y, tiles[edge.B].Y) >= Math.Max(tiles[rect.A].Y, tiles[rect.B].Y);
+    var below = Math.Max(tiles[edge.A].Y, tiles[edge.B].Y) <= Math.Min(tiles[rect.A].Y, tiles[rect.B].Y);
+
+    return !(right || left || above || below);
+}
