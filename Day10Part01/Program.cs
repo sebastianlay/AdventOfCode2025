1dff6a5405c89194e923527624e97c2cdcf4152c
Sebastian Lay <mail@sebastian-lay.de>
2025-12-12 14:08:18 +0100

    Adding day 10

---
+++ Day10Part01/Program.cs
@@ -0,0 +1,70 @@
+﻿var input = File.ReadAllLines("input");
+var machines = input.Select(Parse);
+var min = machines.Sum(Solve);
+
+Console.WriteLine($"Min: {min}");
+return;
+
+static int Solve(Machine machine)
+{
+    var presses = 0;
+
+    while (true)
+    {
+        var empty = new int[machine.Buttons.Length];
+        var combinations = GetCombinations(0, ++presses, empty);
+        if (combinations.Any(combination => ApplyCombination(machine, combination)))
+            return presses;
+    }
+}
+
+static bool ApplyCombination(Machine machine, int[] combination)
+{
+    var appliedLights = new bool[machine.Lights.Length];
+
+    for (var i = 0; i < combination.Length; i++)
+    {
+        if (combination[i] % 2 == 0)
+            continue;
+
+        foreach (var button in machine.Buttons[i])
+            appliedLights[button] = !appliedLights[button];
+    }
+
+    return machine.Lights.SequenceEqual(appliedLights);
+}
+
+static IEnumerable<int[]> GetCombinations(int index, int remaining, int[] values)
+{
+    if (index == values.Length - 1)
+    {
+        values[index] = remaining;
+        yield return values;
+        yield break;
+    }
+
+    for (var take = 0; take <= remaining; take++)
+    {
+        values[index] = take;
+        var combinations = GetCombinations(index + 1, remaining - take, values);
+        foreach (var combination in combinations)
+            yield return combination;
+    }
+}
+
+static Machine Parse(string line)
+{
+    var lights = line[1..line.IndexOf(']')]
+        .Select(c => c == '#')
+        .ToArray();
+
+    var buttons = line[(line.IndexOf(']') + 2)..(line.IndexOf('{') - 1)]
+        .Split(' ')
+        .Select(b => b[1..^1].Split(','))
+        .Select(b => b.Select(int.Parse).ToArray())
+        .ToArray();
+
+    return new Machine(lights, buttons);
+}
+
+internal sealed record Machine(bool[] Lights, int[][] Buttons);
---
+++ Day10Part02/Program.cs
@@ -0,0 +1,93 @@
+﻿var input = File.ReadAllLines("input");
+var machines = input.Select(Parse);
+var min = machines.Sum(Solve);
+
+Console.WriteLine($"Min: {min}");
+return;
+
+static long Solve(Machine machine)
+{
+    var precomputed = GetPrecomputedValues(machine);
+    return GetPresses(machine.Counters, precomputed);
+}
+
+static int GetPresses(int[] joltage, Precomputed precomputed)
+{
+    var result = 10_000;
+
+    if (joltage.All(j => j == 0))
+        return 0;
+
+    if (joltage.Any(j => j < 0))
+        return result;
+
+    var parity = string.Join(',', joltage.Select(j => j % 2 == 0));
+    if (!precomputed.Parities.TryGetValue(parity, out var combinations))
+        return result;
+
+    foreach (var combination in combinations)
+    {
+        var precomputedJoltage = precomputed.Joltages[combination];
+        var newJoltage = new int[joltage.Length];
+        for (var i = 0; i < joltage.Length; i++)
+            newJoltage[i] = (joltage[i] - precomputedJoltage[i]) / 2;
+
+        var combinationPresses = BitOperations.PopCount(combination);
+        var presses = GetPresses(newJoltage, precomputed);
+        var combinationResult = combinationPresses + 2 * presses;
+
+        result = Math.Min(combinationResult, result);
+    }
+
+    return result;
+}
+
+static Precomputed GetPrecomputedValues(Machine machine)
+{
+    var parities = new Dictionary<string, List<uint>>();
+    var joltages = new Dictionary<uint, int[]>();
+
+    var combinations = Math.Pow(2, machine.Buttons.Length);
+    for (var combination = 0u; combination < combinations; combination++)
+    {
+        var joltage = new int[machine.Counters.Length];
+        for (var j = 0; j < machine.Buttons.Length; j++)
+        {
+            if ((combination & (1 << j)) <= 0)
+                continue;
+
+            foreach (var button in machine.Buttons[j])
+                joltage[button] += 1;
+        }
+
+        var parity = string.Join(',', joltage.Select(j => j % 2 == 0));
+        if (parities.TryGetValue(parity, out var value))
+            value.Add(combination);
+        else
+            parities.Add(parity, [combination]);
+
+        joltages.Add(combination, joltage);
+    }
+
+    return new Precomputed(parities, joltages);
+}
+
+static Machine Parse(string line)
+{
+    var buttons = line[(line.IndexOf(']') + 2)..(line.IndexOf('{') - 1)]
+        .Split(' ')
+        .Select(b => b[1..^1].Split(','))
+        .Select(b => b.Select(int.Parse).ToArray())
+        .ToArray();
+
+    var counters = line[(line.IndexOf('{') + 1)..^1]
+        .Split(',')
+        .Select(int.Parse)
+        .ToArray();
+
+    return new Machine(buttons, counters);
+}
+
+internal sealed record Machine(int[][] Buttons, int[] Counters);
+
+internal sealed record Precomputed(Dictionary<string, List<uint>> Parities, Dictionary<uint, int[]> Joltages);
