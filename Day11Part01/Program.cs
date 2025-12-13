819dac8454cb288221368199dcdc700d71fda6ec
Sebastian Lay <mail@sebastian-lay.de>
2025-12-13 22:13:10 +0100

    Adding day 11

---
+++ Day11Part01/Program.cs
@@ -0,0 +1,8 @@
+﻿var input = File.ReadAllLines("input");
+var devices = input.ToDictionary(line => line[..3], line => line[5..].Split(' '));
+var paths = GetPaths("you");
+
+Console.WriteLine($"Paths: {paths}");
+return;
+
+int GetPaths(string name) => name == "out" ? 1 : devices[name].Sum(GetPaths);
---
+++ Day11Part02/Program.cs
@@ -0,0 +1,16 @@
+﻿var input = File.ReadAllLines("input");
+var devices = input.ToDictionary(line => line[..3], line => line[5..].Split(' '));
+var cache = new ConcurrentDictionary<(string name, bool dac, bool fft), long>();
+var paths = GetPaths("svr", false, false);
+
+Console.WriteLine($"Paths: {paths}");
+return;
+
+long GetPaths(string name, bool dac, bool fft)
+{
+    if (name == "dac") dac = true;
+    if (name == "fft") fft = true;
+    if (name == "out") return dac && fft ? 1 : 0;
+
+    return cache.GetOrAdd((name, dac, fft), _ => devices[name].Sum(o => GetPaths(o, dac, fft)));
+}
