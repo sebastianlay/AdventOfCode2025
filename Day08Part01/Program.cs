96c6285c80ca49b793e765357d68a76a0f38aadc
Sebastian Lay <mail@sebastian-lay.de>
2025-12-14 00:19:05 +0100

    Cleaning up day 8

--- Day08Part01/Program.cs
+++ Day08Part01/Program.cs
@@ -1,5 +1,5 @@
 ﻿var input = File.ReadAllLines("input");
-var boxes = new List<(int x, int y, int z)>(input.Length);
+var boxes = new List<(int X, int Y, int Z)>(input.Length);
 const int pairs = 1000;
 
 foreach (var line in input)
@@ -8,16 +8,16 @@ foreach (var line in input)
     boxes.Add((split[0], split[1], split[2]));
 }
 
-var distances = new List<(double distance, int from, int to)>(boxes.Count * boxes.Count / 2);
+var distances = new List<(double Distance, int From, int To)>(boxes.Count * boxes.Count / 2);
 var circuits = new List<HashSet<int>>(boxes.Count);
 
 for (var i = 0; i < boxes.Count; i++)
 {
     for (var j = i + 1; j < boxes.Count; j++)
     {
-        var x = Math.Pow(boxes[i].x - boxes[j].x, 2);
-        var y = Math.Pow(boxes[i].y - boxes[j].y, 2);
-        var z = Math.Pow(boxes[i].z - boxes[j].z, 2);
+        var x = Math.Pow(boxes[i].X - boxes[j].X, 2);
+        var y = Math.Pow(boxes[i].Y - boxes[j].Y, 2);
+        var z = Math.Pow(boxes[i].Z - boxes[j].Z, 2);
         var distance = x + y + z;
         distances.Add((distance, i, j));
     }
@@ -25,12 +25,12 @@ for (var i = 0; i < boxes.Count; i++)
     circuits.Add([i]);
 }
 
-distances = [.. distances.OrderBy(d => d.distance)];
+distances = [.. distances.OrderBy(d => d.Distance)];
 
 foreach (var distance in distances.Take(pairs))
 {
-    var fromCircuit = circuits.First(c => c.Contains(distance.from));
-    var toCirciut = circuits.First(c => c.Contains(distance.to));
+    var fromCircuit = circuits.First(c => c.Contains(distance.From));
+    var toCirciut = circuits.First(c => c.Contains(distance.To));
 
     if (fromCircuit != toCirciut)
     {
--- Day08Part02/Program.cs
+++ Day08Part02/Program.cs
@@ -1,5 +1,5 @@
 ﻿var input = File.ReadAllLines("input");
-var boxes = new List<(int x, int y, int z)>(input.Length);
+var boxes = new List<(int X, int Y, int Z)>(input.Length);
 
 foreach (var line in input)
 {
@@ -7,26 +7,26 @@ foreach (var line in input)
     boxes.Add((split[0], split[1], split[2]));
 }
 
-var distances = new PriorityQueue<(int from, int to), double>(boxes.Count * boxes.Count / 2);
+var distances = new PriorityQueue<(int From, int To), double>(boxes.Count * boxes.Count / 2);
 var circuits = new List<HashSet<int>>(boxes.Count);
 
 for (var i = 0; i < boxes.Count; i++)
 {
     for (var j = i + 1; j < boxes.Count; j++)
     {
-        var x = Math.Pow(boxes[i].x - boxes[j].x, 2);
-        var y = Math.Pow(boxes[i].y - boxes[j].y, 2);
-        var z = Math.Pow(boxes[i].z - boxes[j].z, 2);
+        var x = Math.Pow(boxes[i].X - boxes[j].X, 2);
+        var y = Math.Pow(boxes[i].Y - boxes[j].Y, 2);
+        var z = Math.Pow(boxes[i].Z - boxes[j].Z, 2);
         distances.Enqueue((i, j), x + y + z);
     }
 
     circuits.Add([i]);
 }
 
-while (distances.TryDequeue(out var distance, out var _))
+while (distances.TryDequeue(out var distance, out _))
 {
-    var fromCircuit = circuits.First(c => c.Contains(distance.from));
-    var toCirciut = circuits.First(c => c.Contains(distance.to));
+    var fromCircuit = circuits.First(c => c.Contains(distance.From));
+    var toCirciut = circuits.First(c => c.Contains(distance.To));
 
     if (fromCircuit != toCirciut)
     {
@@ -35,7 +35,7 @@ while (distances.TryDequeue(out var distance, out var _))
 
         if (circuits.Count == 1)
         {
-            var result = (long)boxes[distance.from].x * boxes[distance.to].x;
+            var result = (long)boxes[distance.From].X * boxes[distance.To].X;
             Console.WriteLine($"Result: {result}");
             return;
         }
