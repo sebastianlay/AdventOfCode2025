9e57555cf89f07426de1a7ddb1e375afa61826ab
Sebastian Lay <mail@sebastian-lay.de>
2025-12-14 00:11:07 +0100

    Cleaning up day 6

--- Day06Part01/Program.cs
+++ Day06Part01/Program.cs
@@ -1,26 +1,16 @@
-﻿var input = File.ReadAllLines("input");
-const StringSplitOptions options = StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries;
+﻿const StringSplitOptions options = StringSplitOptions.RemoveEmptyEntries;
+var input = File.ReadAllLines("input");
 var grid = input.Select(line => line.Split(' ', options)).ToArray();
-var width = grid[0].Length;
-var height = grid.Length;
 var total = 0L;
 
-for (var x = 0; x < width; x++)
+for (var x = 0; x < grid[0].Length; x++)
 {
     var operand = grid[^1][x][0];
-    var subtotal = long.Parse(grid[0][x], CultureInfo.InvariantCulture);
-    for (var y = 1; y < height - 1; y++)
-    {
-        switch (operand)
-        {
-            case '+':
-                subtotal += long.Parse(grid[y][x], CultureInfo.InvariantCulture);
-                break;
-            case '*':
-                subtotal *= long.Parse(grid[y][x], CultureInfo.InvariantCulture);
-                break;
-        }
-    }
+    var subtotal = long.Parse(grid[0][x]);
+    for (var y = 1; y < grid.Length - 1; y++)
+        subtotal = operand == '+'
+            ? subtotal + long.Parse(grid[y][x])
+            : subtotal * long.Parse(grid[y][x]);
 
     total += subtotal;
 }
--- Day06Part02/Program.cs
+++ Day06Part02/Program.cs
@@ -1,42 +1,27 @@
 ﻿var input = File.ReadAllLines("input");
-const int height = 4;
-
-// parsing the operations
-var operations = new List<(bool isAdd, int width)>();
-var searchValues = SearchValues.Create("+*");
-var operands = input[height].AsSpan();
-int index;
-var cursor = 1;
-var isAdd = operands[0] == '+';
-while ((index = operands[cursor..].IndexOfAny(searchValues)) >= 0)
-{
-    operations.Add((isAdd, index));
-    isAdd = operands[cursor + index] == '+';
-    cursor += index + 1;
-}
-operations.Add((isAdd, operands.Length - cursor + 1));
-
-// doing the actual calculations
+var numbers = new List<int>();
 var total = 0L;
-var offset = 0;
-foreach (var operation in operations)
+
+for (var i = input[0].Length - 1; i >= 0; i--)
 {
-    var subtotal = operation.isAdd ? 0L : 1L;
-    for (var i = 0; i < operation.width; i++)
+    var concatenated = string.Empty;
+    for (var j = 0; j < input.Length - 1; j++)
+        concatenated += input[j][i];
+
+    numbers.Add(int.Parse(concatenated));
+
+    var operand = input[^1][i];
+
+    if (operand == '+')
+        total += numbers.Sum();
+    else if (operand == '*')
+        total += numbers.Aggregate(1L, (x, y) => x * y);
+
+    if (operand != ' ')
     {
-        var number = string.Empty;
-        for (var j = 0; j < height; j++)
-            number += input[j][i + offset];
-
-        var parsed = long.Parse(number, CultureInfo.InvariantCulture);
-        if (operation.isAdd)
-            subtotal += parsed;
-        else
-            subtotal *= parsed;
+        numbers.Clear();
+        i--;
     }
-
-    total += subtotal;
-    offset += operation.width + 1;
 }
 
 Console.WriteLine($"Total: {total}");
