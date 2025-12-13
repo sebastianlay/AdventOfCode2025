a3d80f9454d4a95249ced355256030cad8bea055
Sebastian Lay <mail@sebastian-lay.de>
2025-12-13 23:01:41 +0100

    Adding day 12

---
+++ Day12Part01/Program.cs
@@ -0,0 +1,23 @@
+﻿var input = File.ReadAllLines("input").AsSpan();
+var lastEmpty = input.LastIndexOf(string.Empty);
+
+var areas = new List<int>();
+var shapes = input[..lastEmpty];
+foreach (var range in shapes.Split(string.Empty))
+    areas.Add(input[range].ToArray().Sum(shape => shape.Count(c => c == '#')));
+
+var result = 0;
+var regions = input[(lastEmpty + 1)..];
+foreach (var region in regions)
+{
+    var area = int.Parse(region[..2]) * int.Parse(region[3..5]);
+    var necessary = 0;
+    var indexes = region[7..].Split(' ').Select(int.Parse).ToArray();
+    for (var i = 0; i < indexes.Length; i++)
+        necessary += indexes[i] * areas[i];
+
+    if (necessary <= area)
+        result++;
+}
+
+Console.WriteLine($"Result: {result}");
